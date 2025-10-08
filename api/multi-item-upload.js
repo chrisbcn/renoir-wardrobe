bf0770c8a819365f3bcaf9265bdd23c972eb6b8d
chrisbcn <chris@comediadesign.com>
2025-10-08 08:13:20 -0700

    Update multi-item-upload.js

--- api/multi-item-upload.js
+++ api/multi-item-upload.js
@@ -1,6 +1,6 @@
 // api/multi-item-upload.js
 import { createClient } from '@supabase/supabase-js';
-
+import crypto from 'crypto';
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
